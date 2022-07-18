930811ea487c7a80d5e65b14e57c9a0b2deeed88
prithiviraj-pisystems <prithiviraj@pi-systems.in>
2022-07-18 21:22:28 +0530

    addedBeforeFood and autoCompleteGetFunctions for impression and plan

---
+++ PiHealth/Migrations/20220705130442_Prescription_AddBefore_Column_Added.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PiHealth.Migrations
+{
+    public partial class Prescription_AddBefore_Column_Added : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "AfterFood",
+                table: "TemplatePrescription",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "AfterFood",
+                table: "Prescription",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AfterFood",
+                table: "TemplatePrescription");
+
+            migrationBuilder.DropColumn(
+                name: "AfterFood",
+                table: "Prescription");
+        }
+    }
+}
